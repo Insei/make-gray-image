aff59da2587a5a9831aa06a490a3bac2994cb28c
Vlad <vladis.osminin@gmail.com>
2022-10-03 11:50:17 +0300

    added new Services for API work

--- MakeGreyImageAPI/Program.cs
+++ MakeGreyImageAPI/Program.cs
@@ -1,10 +1,19 @@
+using System.Reflection;
+using MakeGreyImageAPI.Infrastructure.Context;
+using MakeGreyImageAPI.Infrastructure.Generics;
 using MakeGreyImageAPI.Interfaces;
 using MakeGreyImageAPI.Managers;
+using MakeGreyImageAPI.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddScoped<IImageManager, ImageManager>();
+builder.Services.AddDbContext<DataDbContext>(options =>
+    options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ImageBase;Trusted_Connection=True;"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddTransient<IImageManager, ImageManager>();
+builder.Services.AddTransient<IGenericRepository, GenericRepository>();
+builder.Services.AddTransient<ImageService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
@@ -39,7 +48,7 @@ app.UseRouting();
 app.UseStaticFiles();
 
 app.UseAuthorization();
-
+SeedDataDb.Initialize(app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope().ServiceProvider);
 app.MapControllers();
 
 app.Run();
