f679818d320bd84ed82273807b35994674744609
Vlad <vladis.osminin@gmail.com>
2022-10-05 23:54:45 +0300

    rename from ConversationTaskImageDto to LocalImageConvertTaskDTO

--- MakeGreyImageAPI.DTOs/LocalImageConvertTaskDTO.cs
+++ MakeGreyImageAPI.DTOs/LocalImageConvertTaskDTO.cs
@@ -1,10 +1,10 @@
 namespace MakeGreyImageAPI.DTOs;
 
-public class ConversationTaskImageDto 
+public class LocalImageConvertTaskDTO 
 {
     public Guid Id { get; set; }
     public Guid InImageId { get; set; }
     public Guid? OutImageId { get; set; }
-    public ConvertParameters Parameters { get; set; }
+    public ConvertParameters? Parameters { get; set; }
     public string Status { get; set; } = "";
 }
