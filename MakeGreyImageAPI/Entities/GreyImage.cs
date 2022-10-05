b0e54f9338a135efd625b9a373ea40cc1c01c2f7
Vlad <vladis.osminin@gmail.com>
2022-10-05 23:58:41 +0300

    added GreyImage entity

---
+++ MakeGreyImageAPI/Entities/GreyImage.cs
@@ -0,0 +1,16 @@
+namespace MakeGreyImageAPI.Entities;
+/// <summary>
+/// 
+/// </summary>
+public class GreyImage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public byte[]? Image { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public Guid Id { get; set; }
+}
