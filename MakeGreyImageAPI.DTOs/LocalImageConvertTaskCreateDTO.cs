8359c5ffbe495167b3726a4c37077480c6360adf
Vlad <vladis.osminin@gmail.com>
2022-10-07 18:26:48 +0400

    commented unused string

--- MakeGreyImageAPI.DTOs/LocalImageConvertTaskCreateDTO.cs
+++ MakeGreyImageAPI.DTOs/LocalImageConvertTaskCreateDTO.cs
@@ -3,5 +3,5 @@ namespace MakeGreyImageAPI.DTOs;
 public class LocalImageConvertTaskCreateDTO
 {
     public Guid ImageId { get; set; }
-    public ConvertParameters Parameters { get; set; }
+    // public ConvertParameters Parameters { get; set; }
 }
