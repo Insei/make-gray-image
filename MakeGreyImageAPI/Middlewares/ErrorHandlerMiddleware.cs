08934f13353094fc214f93fd935299039a31c481
Vlad <vladis.osminin@gmail.com>
2022-10-18 15:08:25 +0300

    added comments for ErrorHandlerMiddleware

--- MakeGreyImageAPI/Middlewares/ErrorHandlerMiddleware.cs
+++ MakeGreyImageAPI/Middlewares/ErrorHandlerMiddleware.cs
@@ -2,23 +2,23 @@ using System.Net;
 
 namespace MakeGreyImageAPI.Middlewares;
 /// <summary>
-/// 
+/// Middleware for global error handling
 /// </summary>
 public class ErrorHandlerMiddleware
 {
     private readonly RequestDelegate _next;
     /// <summary>
-    /// 
+    /// constructor of ErrorHandlerMiddleware
     /// </summary>
-    /// <param name="next"></param>
+    /// <param name="next">RequestDelegate</param>
     public ErrorHandlerMiddleware(RequestDelegate next)
     {
         _next = next;
     }
     /// <summary>
-    /// 
+    /// Middleware call point
     /// </summary>
-    /// <param name="context"></param>
+    /// <param name="context">HttpContext</param>
     public async Task Invoke(HttpContext context)
     {
         try
