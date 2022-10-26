eaff15c4c81c61129a6f282eccf03c7265952075
Vlad <vladis.osminin@gmail.com>
2022-10-26 13:54:51 +0300

    added user auditing for SaveChanges method in DataDbContext

--- MakeGreyImageAPI/Infrastructure/Context/DataDbContext.cs
+++ MakeGreyImageAPI/Infrastructure/Context/DataDbContext.cs
@@ -62,4 +62,30 @@ public sealed class DataDbContext : IdentityDbContext<ApplicationUser, IdentityR
         }
         return base.SaveChangesAsync(cancellationToken);
     }
+    /// <summary>
+    /// Method which called when entity saved
+    /// </summary>
+    /// <returns>The number of state entries written to the database</returns>
+    public override int SaveChanges()
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.Created = DateTime.UtcNow;
+                    var createUserId = _authenticatedService.GetUserId();
+                    if(Guid.Empty != createUserId)
+                        entry.Entity.CreatedBy = createUserId;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.Updated = DateTime.UtcNow;
+                    var updateUserId = _authenticatedService.GetUserId();
+                    if(Guid.Empty != updateUserId)
+                        entry.Entity.UpdatedBy = updateUserId;
+                    break;
+            }
+        }
+        return base.SaveChanges();
+    }
 }
