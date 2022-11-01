de1786fcb83414bf8062bb5ed73676049d932d38
Vlad <vladis.osminin@gmail.com>
2022-11-01 19:53:44 +0300

    remade the BaseEntity class by adding the KeyEntity entity to it *inherited KeyEntity to BaseEntity

--- MakeGreyImageAPI/Entities/BaseEntity.cs
+++ MakeGreyImageAPI/Entities/BaseEntity.cs
@@ -1,13 +1,39 @@
+using MakeGreyImageAPI.Interfaces;
+
 namespace MakeGreyImageAPI.Entities;
+
 /// <summary>
-/// Entity class with information about actions with the account
+/// A class containing a field of special entity key
 /// </summary>
-public class BaseEntity
+/// <typeparam name="TKey">Type of special entity key</typeparam>
+public class KeyEntity<TKey> : IKeyEntity<TKey> where TKey :  IComparable, IComparable<TKey>, IEquatable<TKey>
 {
     /// <summary>
     /// Id of the entity
     /// </summary>
-    public virtual Guid Id { get; set; }
+#pragma warning disable CS8618
+    public TKey Id { get; set; }
+#pragma warning restore CS8618
+}
+/// <summary>
+/// Entity class with information about actions with the account
+/// </summary>
+public class BaseEntity<TKey> : IKeyEntity<TKey>, IUserTrackedEntity, ITimeTrackedEntity where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
+{
+    /// <summary>
+    /// Id of the entity
+    /// </summary>
+#pragma warning disable CS8618
+    public TKey Id { get; set; }
+#pragma warning restore CS8618
+    /// <summary>
+    /// Create by user ID
+    /// </summary>
+    public Guid CreatedBy { get; set; }
+    /// <summary>
+    /// Last modified by user ID
+    /// </summary>
+    public Guid? UpdatedBy { get; set; }
     /// <summary>
     /// Created at DateTime UTC
     /// </summary>
@@ -16,12 +42,4 @@ public class BaseEntity
     /// Last modified DateTime UTC
     /// </summary>
     public DateTime? Updated { get; set; }
-    /// <summary>
-    /// Create by user ID
-    /// </summary>
-    public virtual Guid CreatedBy { get; set; }
-    /// <summary>
-    /// Last modified by user ID
-    /// </summary>
-    public Guid? UpdatedBy { get; set; }
 }
